86f02fbedf769e31158afe78dbedf147789aee80
HoangHaix86 <hai.hoang.2762@gmail.com>
2023-07-31 01:26:47 +0700

    update

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # https://github.com/HoangHaix86/setup-hadoop.git
-# rm -rf setup-hadoop git clone https://github.com/HoangHaix86/setup-hadoop.git chmod +x ./setup-hadoop/script.sh sudo ./setup-hadoop/script.sh
+# rm -rf setup-hadoop && git clone https://github.com/HoangHaix86/setup-hadoop.git && chmod +x ./setup-hadoop/script.sh && sudo ./setup-hadoop/script.sh
 
 IP_MASTER=""
 IP_SLAVE_1=""
@@ -9,34 +9,36 @@ IP_SLAVE_2=""
 IP_SLAVE_3=""
 
 # Update vs upgrade
-sudo apt-get update sudo apt-get upgrade -y
+sudo apt update
+sudo apt upgrade -y
 
 # for setup
-sudo apt install -y open-vm-tools-desktop
+# sudo apt install -y open-vm-tools-desktop
 
 # Install openssh-server
-sudo apt-get install -y openssh-server python3-lxml openjdk-8-jdk git wget
+sudo apt install -y openssh-server python3-lxml openjdk-8-jdk git
 
 # ssh
-ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa -y
+rm ~/.ssh/id_rsa
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub >>~/.ssh/authorized_keys
 sudo chmod 0600 ~/.ssh/authorized_keys
 
 # add xmleditor
 sudo cp ./**/xmleditor.py /usr/local/bin/xmleditor.py
-chmod a+x /usr/local/bin/xmleditor.py
+sudo chmod a+x /usr/local/bin/xmleditor.py
 
 sudo chmod -R a+rwx /opt
 
 export HADOOP_VERSION=3.3.6
 export HADOOP_HOME=/opt/hadoop
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=\$PATH:\$JAVA_HOME/bin
+export PATH=$PATH:$JAVA_HOME/bin
 
 export HADOOP_LOG_DIR=\$HADOOP_HOME/logs
 export HADOOP_CONF_DIR=\$HADOOP_HOME/etc/hadoop
-export PATH=\$PATH:\$HADOOP_HOME/bin
-export PATH=\$PATH:\$HADOOP_HOME/sbin
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
 export HADOOP_MAPRED_HOME=\$HADOOP_HOME
 export HADOOP_COMMON_HOME=\$HADOOP_HOME
 export HADOOP_HDFS_HOME=\$HADOOP_HOME
@@ -58,11 +60,10 @@ echo "export YARN_HOME=\$HADOOP_HOME" >>~/.bashrc
 source ~/.bashrc
 
 # install hadoop
-wget "https://dlcdn.apache.org/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz"
-tar -xvzf hadoop-$HADOOP_VERSION.tar.gz
-mv -f hadoop-$HADOOP_VERSION $HADOOP_HOME
-rm hadoop-$HADOOP_VERSION.tar.gz
-# add environment variables
+wget "https://dlcdn.apache.org/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz" &&
+tar -xvzf hadoop-$HADOOP_VERSION.tar.gz &&
+sudo mv -f hadoop-$HADOOP_VERSION $HADOOP_HOME &&
+rm hadoop-$HADOOP_VERSION.tar.gz &&
 echo "export JAVA_HOME=\$JAVA_HOME" >>$HADOOP_HOME/etc/hadoop/hadoop-env.sh
 
 # add host
@@ -73,24 +74,24 @@ echo "export JAVA_HOME=\$JAVA_HOME" >>$HADOOP_HOME/etc/hadoop/hadoop-env.sh
 
 # # config hadoop
 # # core-site.xml
-# xmleditor.py -a -p $HADOOP_CONF_DIR/core-site.xml -n fs.defaultFS -v hdfs://master:9000
+xmleditor.py -a -p $HADOOP_CONF_DIR/core-site.xml -n fs.defaultFS -v hdfs://master:9000
 
 # # hdfs-site.xml
-# xmleditor.py -a -p $HADOOP_CONF_DIR/hdfs-site.xml -n dfs.replication -v 3
-# xmleditor.py -a -p $HADOOP_CONF_DIR/hdfs-site.xml -n dfs.namenode.name.dir -v file:///opt/hadoop/data/namenode
-# xmleditor.py -a -p $HADOOP_CONF_DIR/hdfs-site.xml -n dfs.datanode.data.dir -v file:///opt/hadoop/data/datanode
+xmleditor.py -a -p $HADOOP_CONF_DIR/hdfs-site.xml -n dfs.replication -v 3
+xmleditor.py -a -p $HADOOP_CONF_DIR/hdfs-site.xml -n dfs.namenode.name.dir -v file:///opt/hadoop/data/namenode
+xmleditor.py -a -p $HADOOP_CONF_DIR/hdfs-site.xml -n dfs.datanode.data.dir -v file:///opt/hadoop/data/datanode
 
 # # yarn-site.xml
-# xmleditor.py -a -p $HADOOP_CONF_DIR/yarn-site.xml -n yarn.nodemanager.aux-services -v mapreduce_shuffle
-# xmleditor.py -a -p $HADOOP_CONF_DIR/yarn-site.xml -n yarn.nodemanager.aux-services.mapreduce.shuffle.class -v org.apache.hadoop.mapred.ShuffleHandler
-# xmleditor.py -a -p $HADOOP_CONF_DIR/yarn-site.xml -n yarn.resourcemanager.hostname -v master
+xmleditor.py -a -p $HADOOP_CONF_DIR/yarn-site.xml -n yarn.nodemanager.aux-services -v mapreduce_shuffle
+xmleditor.py -a -p $HADOOP_CONF_DIR/yarn-site.xml -n yarn.nodemanager.aux-services.mapreduce.shuffle.class -v org.apache.hadoop.mapred.ShuffleHandler
+xmleditor.py -a -p $HADOOP_CONF_DIR/yarn-site.xml -n yarn.resourcemanager.hostname -v master
 
 # # mapred-site.xml
-# xmleditor.py -a -p $HADOOP_CONF_DIR/mapred-site.xml -n mapreduce.framework.name -v yarn
-# xmleditor.py -a -p $HADOOP_CONF_DIR/mapred-site.xml -n mapreduce.jobtracker.address -v master:54311
+xmleditor.py -a -p $HADOOP_CONF_DIR/mapred-site.xml -n mapreduce.framework.name -v yarn
+xmleditor.py -a -p $HADOOP_CONF_DIR/mapred-site.xml -n mapreduce.jobtracker.address -v master:54311
 
-# sudo mkdir -p /opt/hadoop/data/namenode
-#     sudo mkdir -p /opt/hadoop/data/datanode
+sudo mkdir -p /opt/hadoop/data/namenode
+sudo mkdir -p /opt/hadoop/data/datanode
 
 # # master
 # # echo master >$HADOOP_HOME/etc/hadoop/masters
