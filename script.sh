36e55562e89cf590af634910a55c1174ec9326e9
HoangHaix86 <hai.hoang.2762@gmail.com>
2023-07-30 22:59:15 +0700

    update any

--- script.sh
+++ script.sh
@@ -28,19 +28,18 @@ sudo chmod +x /usr/local/bin/xmleditor.py
 
 sudo chmod -R a+rwx /opt
 
-export HADOOP_VERSION=3.3.6
-export HADOOP_HOME=/opt/hadoop
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=\$PATH:\$JAVA_HOME/bin
-
-export HADOOP_LOG_DIR=\$HADOOP_HOME/logs
-export HADOOP_CONF_DIR=\$HADOOP_HOME/etc/hadoop
-export PATH=\$PATH:\$HADOOP_HOME/bin
-export PATH=\$PATH:\$HADOOP_HOME/sbin
-export HADOOP_MAPRED_HOME=\$HADOOP_HOME
-export HADOOP_COMMON_HOME=\$HADOOP_HOME
-export HADOOP_HDFS_HOME=\$HADOOP_HOME
-export YARN_HOME=\$HADOOP_HOME
+HADOOP_VERSION=3.3.6
+HADOOP_HOME=/opt/hadoop
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+PATH=\$PATH:\$JAVA_HOME/bin
+HADOOP_LOG_DIR=\$HADOOP_HOME/logs
+HADOOP_CONF_DIR=\$HADOOP_HOME/etc/hadoop
+PATH=\$PATH:\$HADOOP_HOME/bin
+PATH=\$PATH:\$HADOOP_HOME/sbin
+HADOOP_MAPRED_HOME=\$HADOOP_HOME
+HADOOP_COMMON_HOME=\$HADOOP_HOME
+HADOOP_HDFS_HOME=\$HADOOP_HOME
+YARN_HOME=\$HADOOP_HOME
 
 echo "export HADOOP_VERSION=3.3.6" >>~/.bashrc
 echo "export HADOOP_HOME=/opt/hadoop" >>~/.bashrc
@@ -55,8 +54,6 @@ echo "export HADOOP_MAPRED_HOME=\$HADOOP_HOME" >>~/.bashrc
 echo "export HADOOP_COMMON_HOME=\$HADOOP_HOME" >>~/.bashrc
 echo "export HADOOP_HDFS_HOME=\$HADOOP_HOME" >>~/.bashrc
 echo "export YARN_HOME=\$HADOOP_HOME" >>~/.bashrc
-
-which wget
 
 # install hadoop
 URL="https://dlcdn.apache.org/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz"
